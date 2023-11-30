f7ad10fd4bd84226d541d91d89281d564586aa73
PereValiente <147438257+PereValiente@users.noreply.github.com>
2023-11-30 19:12:47 +0100

    Dialogue

--- Animation/Animation.gd
+++ Animation/Animation.gd
@@ -9,13 +9,8 @@ func _ready():
 	#DialogueManager.show_example_dialogue_balloon(load("res://Dialogues/test.dialogue"), "Prueba")
 	face_talk.play("Face_talk")
 	character_talk.play("Character_talk")
+	DialogueManager.show_dialogue_balloon(load("res://Animation/test.dialogue"),"animation_dialogue")
 	
-	"""
-	print(background.scroll_offset.x)
-	if background.scroll_offset.x > 800:
-		face_talk.play("Face_talk")
-		character_talk.play("Character_talk")
-	"""
 
 func _on_timer_timeout():
 	first_animation.visible = !first_animation.visible
@@ -23,4 +18,4 @@ func _on_timer_timeout():
 
 func _on_texture_button_pressed():
 	var dialogue = preload("res://Animation/test.dialogue")
-	#DialogueManager.show_example_dialogue_balloon(animation_dialogue, dialogue)
+	DialogueManager.show_example_dialogue_balloon(dialogue)
