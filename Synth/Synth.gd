2796a71e0e6f1fbe7e54d17c762eb769eb232dac
PereValiente <147438257+PereValiente@users.noreply.github.com>
2023-12-03 13:12:12 +0100

    cosas

--- Synth/Synth.gd
+++ Synth/Synth.gd
@@ -2,10 +2,12 @@ extends Node
 
 #@export var scene_settings : PackedScene
 
-var playback # Will hold the AudioStreamGeneratorPlayback.
-@onready var sample_rate = $AudioStreamPlayer.stream.mix_rate #muestras por segundo
+var playback : AudioStreamGeneratorPlayback # Will hold the AudioStreamGeneratorPlayback.
+@onready var sample_rate = $Synth_audio_1.stream.mix_rate #muestras por segundo (44100)
+@onready var synth_audio_1 = $Synth_audio_1
 var sample_wave = 4096 
 var total_sample_envelope = 0
+var total_sample_ads = 0
 var previous_sample = 0.0
 var difference_filter_sample = 0.0
 var dpw_sample = 0.0
@@ -52,7 +54,8 @@ func _on_set_sound_state(frequency:float, active:bool):
 		f = frequency
 		play_sound()
 	else:
-		$AudioStreamPlayer.stop()
+		oscillator_release()
+#		synth_audio_1.stop()
 
 
 func oscillator():
@@ -66,7 +69,8 @@ func oscillator():
 		correction_amplitude_filter = 20
 	else:
 		correction_amplitude_filter = 1
-	for i in range(total_sample_envelope): 
+	for i in range(total_sample_ads): 
+	#for i in range(total_sample_envelope): 
 		# Obtener cuatro puntos consecutivos para la interpolación cúbica
 		var p0 = table[int(index) % sample_wave]
 		var p1 = table[int(index + 1) % sample_wave]
@@ -86,26 +90,74 @@ func oscillator():
 		playback.push_frame(Vector2.ONE * output[i])
 
 
-func get_adsr_envelope(current_frame):
-	var attack_frames = int(adsr_attack * sample_rate)
-	var decay_frames = int(adsr_decay * sample_rate)
+func oscillator_release():
+	var release_frames: float = adsr_release * sample_rate
+	
+	playback.clear_buffer()
+	
+	var index = 0 #Get real index - With start time
+	var index_increment = (f * sample_wave) / sample_rate
+	var table = wave_tables[wave_type]
+	var gain_dB = -20
+	var amplitude = pow(10,gain_dB/20)
+	
+	if filter_value < 1000:
+		correction_amplitude_filter = 20
+	else:
+		correction_amplitude_filter = 1
+	
+	var envelope
+	for i in range(total_sample_ads): 
+	#for i in range(total_sample_envelope): 
+		# Obtener cuatro puntos consecutivos para la interpolación cúbica
+		var p0 = table[int(index) % sample_wave]
+		var p1 = table[int(index + 1) % sample_wave]
+		var p2 = table[int(index + 2) % sample_wave]
+		var p3 = table[int(index + 3) % sample_wave]
+		# Calcular el parámetro de interpolación (p) entre 0 y 1
+		var p = index - int(index)
+		output.append(0.0)
+		envelope = get_r_envelope(i)
+		output[i] = cubic_interpolate(p0,p1,p2,p3,p)
+		index+=index_increment
+		index = int(index + index_increment) % sample_wave
+		if wave_type == 3: #DPW algorythm for sawtooth
+			dpw_algorithm(output[i])
+			output[i] = dpw_sample
+		output[i] *= amplitude * envelope * correction_amplitude_filter #aplicar a la salida el valor de amplitud y envolvente
+		playback.push_frame(Vector2.ONE * output[i])
+
+func get_adsr_envelope(current_frame: float):
+	var attack_frames: float = adsr_attack * sample_rate
+	var decay_frames: float = adsr_decay * sample_rate
 	var sustain_frames = int(adsr_sustain * sample_rate)
-	var release_frames = int(adsr_release * sample_rate)
-	total_sample_envelope = attack_frames + decay_frames + sustain_frames + release_frames
+	var release_frames: float = adsr_release * sample_rate
+	#total_sample_envelope = attack_frames + decay_frames + sustain_frames + release_frames
+	total_sample_ads = attack_frames + decay_frames + sustain_frames
 
 	if current_frame < attack_frames:
-		return float(current_frame) / float(attack_frames)
+		return current_frame / attack_frames
 	elif current_frame < (attack_frames + decay_frames):
-		return 1.0 - (1.0 - adsr_sustain) * float(current_frame - attack_frames) / float(decay_frames)
-	elif current_frame < (total_sample_envelope - release_frames):
+		return 1.0 - (1.0 - adsr_sustain) * (current_frame - attack_frames) / decay_frames
+	elif current_frame < total_sample_ads:
+	#elif current_frame < (total_sample_envelope - release_frames):
 		return adsr_sustain
-	else:
-		return adsr_sustain * (1.0 - float(current_frame - (total_sample_envelope - release_frames)) / float(release_frames))
+	#else:
+	#	return adsr_sustain * (1.0 - float(current_frame - (total_sample_envelope - release_frames)) / release_frames)
+
+
+func get_r_envelope (current_frame: float):
+	var attack_frames: float = adsr_attack * sample_rate
+	var decay_frames: float = adsr_decay * sample_rate
+	var sustain_frames = int(adsr_sustain * sample_rate)
+	var release_frames: float = adsr_release * sample_rate
+	total_sample_envelope = attack_frames + decay_frames + sustain_frames + release_frames
+	return adsr_sustain * (1.0 - float(current_frame - (total_sample_envelope - release_frames)) / release_frames)
 
 
 func play_sound():
-	$AudioStreamPlayer.play()
-	playback = $AudioStreamPlayer.get_stream_playback()
+	synth_audio_1.play()
+	playback = synth_audio_1.get_stream_playback()
 	oscillator()
 
 
