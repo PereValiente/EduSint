1c537520ef72071d2e3b1f9458ac3a6ab04387d8
PereValiente <147438257+PereValiente@users.noreply.github.com>
2024-01-18 11:54:37 +0100

    connecting menu to synthesizer

--- Main_menu/src/menu_controller.gd
+++ Main_menu/src/menu_controller.gd
@@ -5,6 +5,7 @@ class_name Menu
 
 @export var settings : Settings
 @export var scene_animation : PackedScene
+@export var synth_and_settings_scene : PackedScene
 @onready var panel_settings = $"../Panel_Settings"
 
 
@@ -12,12 +13,12 @@ func _ready():
 	settings.button_settings.visible = !settings.button_settings.visible
 
 
-func _on_select_pressed():
-	panel_settings.visible = !panel_settings.visible
-
-
 func _on_start_button_pressed():
 	get_tree().change_scene_to_packed(scene_animation)
+
+
+func _on_synthesizer_button_pressed():
+	get_tree().change_scene_to_packed(synth_and_settings_scene)
 
 
 func _on_option_button_pressed():
@@ -26,6 +27,4 @@ func _on_option_button_pressed():
 
 func _on_exit_button_pressed():
 	get_tree().quit()
-
-
 
